4d00d2b45976a7ac1b7c85e3a861aa49360bfbf4
Roman Sevko <k6-2@ya.ru>
2017-02-09 19:11:33 +0300

    Add files via upload

---
+++ linuxcmd/attributes/default.rb
@@ -0,0 +1,61 @@
+#
+# Cookbook:: linuxcmd
+# Attributes:: default
+#
+# Copyright:: 2017, apple_rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+myuser="admin"
+myhome="/home/admin"
+
+node['etc']['passwd'].each do |user, data|
+    if data['dir'].start_with?("/home/")
+        myuser=user
+        myhome=data['dir']
+    end
+end
+default['my']['user'] = myuser
+default['my']['home'] = myhome
+
+=begin
+default['myuser'] =
+  case node['platform_family']
+  when 'debian'
+    case node['platform']
+    when 'ubuntu'
+      if node['platform_version'].to_f >= 14.04
+        'event'
+      elsif node['platform_version'].to_f >= 12.04
+        'worker'
+      else
+        'prefork'
+      end
+    when 'debian'
+      node['platform_version'].to_f >= 7.0 ? 'worker' : 'prefork'
+    when 'linuxmint'
+      node['platform_version'].to_i >= 17 ? 'event' : 'prefork'
+    else
+      'prefork'
+    end
+  when 'suse'
+    'prefork'
+  when 'rhel'
+    'prefork'
+  else
+    'prefork'
+  end
+=end
+
+
