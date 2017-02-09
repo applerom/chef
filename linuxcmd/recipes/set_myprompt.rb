3ec10047201b89434c7226b7dc0704c01a3ab226
Roman Sevko <k6-2@ya.ru>
2017-02-09 13:16:00 +0300

    Add files via upload

--- linuxcmd/recipes/set_myprompt.rb
+++ linuxcmd/recipes/set_myprompt.rb
@@ -25,8 +25,10 @@ node['etc']['passwd'].each do |user, data|
         myhome=data['dir']
     end
 end
+Chef::Log.debug("myuser = #{myuser}, myhome = #{myhome}")
 
 bashrc = myhome + "/.bashrc"
+Chef::Log.debug("bashrc = #{bashrc}, myhome = #{myhome}")
 
 bashrc_orig = File.read(bashrc)
 
@@ -36,3 +38,5 @@ template bashrc do
         bashrc_orig_content: bashrc_orig
     })
 end unless File.open(bashrc).grep(/myprompt/)
+
+Chef::Log.debug("grep = #{File.open(bashrc).grep(/myprompt/)}")
