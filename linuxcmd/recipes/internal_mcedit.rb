fea1bf16ded4af10ffbadbd6385b0298bc2040b3
Roman Sevko <k6-2@ya.ru>
2017-02-10 19:22:04 +0300

    Add files via upload

---
+++ linuxcmd/recipes/internal_mcedit.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook:: linuxcmd
+# Recipe:: internal_mcedit
+#
+# Copyright:: 2017, apple_rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+mc_xdg="#{node.default['my']['mc_xdg']}"
+Chef::Log.info("mc_xdg = #{mc_xdg}")
+
+myhome="#{node.default['my']['home']}"
+
+ruby_block "mc_version" do
+    block do
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)      
+        command = 'mc -V'
+        command_out = shell_out(command)
+        node.set['my']['mc_version'] = command_out.stdout
+    end
+    action :create
+end
+
+Chef::Log.info("mc_version = #{node['my']['mc_version']}")
