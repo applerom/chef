d0e125daea751ed0e8d5623665489b564c5ceeae
Roman Sevko <k6-2@ya.ru>
2017-02-11 11:56:09 +0300

    Add files via upload

--- linuxcmd/recipes/internal_mcedit.rb
+++ linuxcmd/recipes/internal_mcedit.rb
@@ -29,7 +29,7 @@ ruby_block "mc_version" do
         command = 'mc -V'
         command_out = shell_out(command)
         Chef::Log.info("command_out = #{command_out.stdout}")
-        node.run_state['my']['mc_version'] = "#{command_out.stdout}"
+        node.default.run_state['my']['mc_version'] = "#{command_out.stdout}"
     end
     action :create
 end
