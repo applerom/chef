346519d2c73c05d19acc186822e1c6a0261a5e69
apple_rom <k6-2@ya.ru>
2017-02-15 12:15:18 +0300

    1

---
+++ linuxcmd/recipes/false_shells.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook:: linuxcmd
+# Recipe:: false_shells
+#
+# Copyright:: 2017, apple_rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+file_path = '/etc/shells'
+add_string = '/bin/false'
+
+file_content = File.read(file_path)
+Chef::Log.info("file_content.scan(/#{add_string}/) = #{file_content.scan(/#{add_string}/)}, file_content.scan(/#{add_string}/).length = #{file_content.scan(/#{add_string}/).length}")
+if file_content.scan(/#{add_string}/).length == 0
+    file file_path do
+        content file_content + "\n" + add_string
+    end
+end
---
+++ linuxcmd/recipes/vim_nano.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook:: linuxcmd
+# Recipe:: vim_nano
+#
+# Copyright:: 2017, apple_rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+bash 'vim_nano' do
+    ignore_failure = true
+    code <<-EOF
+        if [ -f /bin/vi_orig ] ; then # protect from repeated running
+            rm /bin/vi                          > /dev/null 2> /dev/null
+            ln -s /usr/bin/nano /bin/vi	        > /dev/null 2> /dev/null
+            rm /usr/bin/vim	                    > /dev/null 2> /dev/null
+            ln -s /usr/bin/nano /usr/bin/vim	> /dev/null 2> /dev/null
+        else
+            mv /bin/vi /bin/vi_orig             > /dev/null 2> /dev/null
+            ln -s /usr/bin/nano /bin/vi         > /dev/null 2> /dev/null
+            mv /usr/bin/vim /usr/bin/vim_orig	> /dev/null 2> /dev/null
+            ln -s /usr/bin/nano /usr/bin/vim	> /dev/null 2> /dev/null
+        fi
+    EOF
+end
+
+
+
