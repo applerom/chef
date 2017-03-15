1d209e932a13d94c912c82c896affc5ade7a6914
applerom <k6-2@ya.ru>
2017-03-15 13:27:24 +0300

    1

--- aws_logger/recipes/install.rb
+++ aws_logger/recipes/install.rb
@@ -3,14 +3,13 @@ directory node["cloudwatchlogs"]["home_dir"] do
 end
 
 if defined?(node['pls']['log_path'])
+    Chef::Log.info("*** log_path (node['pls']['log_path']) defined and is '#{node['pls']['log_path']}' ***")
     log_path = node['pls']['log_path']
-    Chef::Log.info("*** log_path (node['pls']['log_path']) defined and is '#{log_path}' ***")
 else
-    log_path = '/var/log/turn.log'
     Chef::Log.info("*** node['pls']['log_path'] is not defined ***")
+    log_path = '/var/log/turn.log'
 end
 
-Chef::Log.info("*** node['pls']['log_path'] = '#{node['pls']['log_path']}' ***")
 Chef::Log.info("*** log_path = '#{log_path}' ***")
 
 stack = search("aws_opsworks_stack").first
