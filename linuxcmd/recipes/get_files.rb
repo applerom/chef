8976cc67bcf3aa64d007e2a7ba1099a4f1ddd4c5
applerom <k6-2@ya.ru>
2017-04-21 18:12:27 +0300

    1

--- linuxcmd/recipes/get_files.rb
+++ linuxcmd/recipes/get_files.rb
@@ -27,7 +27,7 @@ node['my']['s3_file'].each do |s3_bucket, s3_bucket_files|
         cur_owner = "root"
         cur_group = "root"
         if file_path.kind_of?(Array)
-            %w(cur_file cur_mode cur_owner cur_group).each_with_index do |x,i|
+            [cur_file, cur_mode, cur_owner, cur_group].each_with_index do |x,i|
                 Chef::Log.info("#{x} = file_path[#{i}] = #{file_path[i]}")
                 unless file_path[i].to_s.empty?
                     x = file_path[i]
