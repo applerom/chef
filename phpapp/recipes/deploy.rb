9dc4b33a72cfad8393c1c4cb6aec33dbfe258522
applerom <k6-2@ya.ru>
2017-06-26 18:23:57 +0300

    1

--- phpapp/recipes/deploy.rb
+++ phpapp/recipes/deploy.rb
@@ -40,12 +40,17 @@ end
 Chef::Log.info("node['phpapp']['git_repository']  = '#{node['phpapp']['git_repository']}'")
 Chef::Log.info("node['phpapp']['www_dir']         = '#{node['phpapp']['www_dir']}'")
 
+
 search("aws_opsworks_app").each do |app|
     Chef::Log.info("********** The app is '#{app}' **********")
     Chef::Log.info("********** The app app_id is '#{app['app_id']}' **********")
     Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+    
     s3_path     = app['app_source']['url'].split('.s3.amazonaws.com')[1]
     s3_bucket   = app['app_source']['url'].split('.s3.amazonaws.com')[0].split('://')[1]
+    Chef::Log.info("*** s3_path = '#{s3_path}' **********")
+    Chef::Log.info("*** s3_bucket = '#{s3_bucket}' **********")
+    
     Chef::Log.info("********** The app deploy is '#{app['deploy']}' **********")
     Chef::Log.info("********** The app enable_ssl is '#{app['enable_ssl']}' **********")
     Chef::Log.info("********** The app environment is '#{app['environment']}' **********")
@@ -53,6 +58,9 @@ search("aws_opsworks_app").each do |app|
     Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
     Chef::Log.info("********** The app's type is '#{app['type']}' **********")
 end
+
+s3_path     = search("aws_opsworks_app").first['app_source']['url'].split('.s3.amazonaws.com')[1]
+s3_bucket   = search("aws_opsworks_app").first['app_source']['url'].split('.s3.amazonaws.com')[0].split('://')[1]
 
 Chef::Log.info("s3_path = '#{s3_path}' **********")
 Chef::Log.info("s3_bucket = '#{s3_bucket}' **********")
